0bdc5873e6d3c960c97f230ae4217a0f67a5413c
HabibIbrahimov <habibyi@code.edu.az>
2022-12-16 17:23:33 +0400

    Map function and key

--- src/components/App.js
+++ src/components/App.js
@@ -3,52 +3,53 @@ import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+  state = {
+    movies: [
+      {
+        id: 1,
+        name: 'The Flash',
+        rating: 8.3,
+        overview:
+          'This is a wider card with supporting text below as a natural lead-in to additional content.',
+        imageURL:
+          'https://image.tmdb.org/t/p/w220_and_h330_face/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg',
+      },
 
-    state = {
-        movies: [
-            {
-                "id": 1,
-                "name": "The Flash",
-                "rating": 8.3,
-                "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-                "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg"
-            },
-        
-            {
-                "id": 2,
-                "name": "Interstellar",
-                "rating": 6.8,
-                "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-                "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg"
-            },
-        
-            {
-                "id": 3,
-                "name": "Arrow",
-                "rating": 7.9,
-                "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-                "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/gKG5QGz5Ngf8fgWpBsWtlg5L2SF.jpg"
-            }
-        ]
-    }
+      {
+        id: 2,
+        name: 'Interstellar',
+        rating: 6.8,
+        overview:
+          'This is a wider card with supporting text below as a natural lead-in to additional content.',
+        imageURL:
+          'https://image.tmdb.org/t/p/w220_and_h330_face/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg',
+      },
 
-    render() {
+      {
+        id: 3,
+        name: 'Arrow',
+        rating: 7.9,
+        overview:
+          'This is a wider card with supporting text below as a natural lead-in to additional content.',
+        imageURL:
+          'https://image.tmdb.org/t/p/w220_and_h330_face/gKG5QGz5Ngf8fgWpBsWtlg5L2SF.jpg',
+      },
+    ],
+  };
 
-        return (
-            <div className="container">
-                <div className="row">
-                    <dic className="col-lg-12">
-                        <SearchBar />
-                    </dic>
-                </div>
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <SearchBar />
+          </div>
+        </div>
 
-                <MovieList />
-            </div>
-        )
-
-    }
-
-
+        <MovieList movies={this.state.movies} />
+      </div>
+    );
+  }
 }
 
 export default App;
