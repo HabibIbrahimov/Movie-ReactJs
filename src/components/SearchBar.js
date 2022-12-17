fbdf2a790a977bbdfdd6885102ab2640590141c9
HabibIbrahimov <habibyi@code.edu.az>
2022-12-17 14:05:00 +0400

    Searching movies

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
-class SearchBar extends React.Component{
-    render () {
-        return(
-         <h1>SearchBar</h1>
-        )
-    }
+class SearchBar extends React.Component {
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleFormSubmit}>
+        <div className="form-row mb-5">
+          <div className="col-12">
+            <input
+              onChange={this.props.searchMovieProp}
+              type="text"
+              className="form-control"
+              placeholder="Seach a movie"
+            />
+          </div>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import ReactDom from 'react-dom';
 import App from './components/App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-ReactDom.render(<App/>,document.getElementById('root'));
+ReactDom.render(<App />, document.getElementById('root'));
